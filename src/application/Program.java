148b63ad9016a8c9115a72fba5f1f5f6ee7f1667
Fernando <fernando.luiz92@hotmail.com>
2021-11-20 19:14:33 -0300

    Implementacao de Interface

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import entities.Product;
+import util.UpperCaseName;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
+public class Program {
+    public static void main(String args[]){
+
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("tv",500.00));
+        list.add(new Product("Geladeira",800.00));
+        list.add(new Product("mouse",30.00));
+
+        //a função map aplica a funcao em cada elemento da stream, gerando uma nova stream com elementos transformados
+        List<String> names = list.stream().map(new UpperCaseName()).collect(Collectors.toList());
+        names.forEach(System.out::println);
+    }
+}
